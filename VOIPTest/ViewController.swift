0fe9d587278659bd562a6061cd8146b1e5d98631
LucidioABS <lucidio.abs@gmail.com>
2020-03-26 09:57:25 -0400

    Initial Commit

---
+++ VOIPTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VOIPTest
+//
+//  Created by Lucídio Andrade Barbosa de Souza on 26/03/20.
+//  Copyright © 2020 Lucídio Andrade Barbosa de Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
